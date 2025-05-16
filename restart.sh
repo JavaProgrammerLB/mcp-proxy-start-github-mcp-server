f7396575be9e4fb95721bcb71475c06bafc8e52e
Bill Lau <656860993@qq.com>
2025-05-16 11:55:30 +0800

    fix github mcp server must need GITHUB_PERSONAL_ACCESS_TOKEN issue

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ps aux | grep runGithubMCPStdioServer.sh | grep -v grep | awk '{print $2}' | xargs -r kill -9
 
-nohup mcp-proxy sh ~/Program/mcp-proxy/runGithubMCPStdioServer.sh --port=8085 --host=0.0.0.0 -e MYGITHUB_PERSONAL_ACCESS_TOKEN ${MYGITHUB_PERSONAL_ACCESS_TOKEN} > mcpProxyGithubMCPServer.log 2>&1 &
+nohup mcp-proxy sh ~/Program/mcp-proxy/runGithubMCPStdioServer.sh --port=8085 --host=0.0.0.0 -e GITHUB_PERSONAL_ACCESS_TOKEN ${MYGITHUB_PERSONAL_ACCESS_TOKEN} > mcpProxyGithubMCPServer.log 2>&1 &
